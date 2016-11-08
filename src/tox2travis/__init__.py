301a45385f05c1fae24dad9650add8b06a96e0ef
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-11-08 16:59:31 -0300

    another another

--- src/tox2travis/__init__.py
+++ src/tox2travis/__init__.py
@@ -1,6 +1,17 @@
 __version__ = "0.1.0"
 import sys
 from tox import hookimpl
+import re
+
+python_version_re = re.compile(r'(\/?|\\?|^)(?P<python_version>(pypy3?|python([2-3]\.\d)))($|\.)')
+matrix_template = """matrix:
+  include:
+"""
+
+python_template = """  - python: '{travis_python}'
+    env:
+    - TOX_ENV={tox_env_name}
+"""
 
 
 @hookimpl
@@ -16,10 +27,24 @@ def tox_configure(config):
     from pprint import pprint
 
     if config.option.to_travis:
-        tox_envs = [{'basepython': value.basepython,
-                     'tox_env_name': value.envname,
-                     } for value in config.envconfigs.values()]
+        tox_envs = []
+        v = None
+        for value in config.envconfigs.values():
+            tox_envs.append({'basepython': value.basepython,
+                             'tox_env_name': value.envname,
+                             })
+            v = value
 
-        pprint(tox_envs)
+        d = {f: getattr(v, f) for f in dir(v) if not f.startswith('_')}
+        pprint(d)
+
+        # pprint(tox_envs)
+
+        for v in tox_envs:
+            g = 'Nada'
+            r = python_version_re.search(v.get('basepython'))
+            if r:
+                g = r.group('python_version')
+            # print(v.get('tox_env_name'), v.get('basepython'), 'regex match value', g)
 
         sys.exit(0)
